7191c09453c7c27f9101d89701d887df2768f041
宋军鹏 <sjp@xinlonghang.cn>
2017-09-26 16:47:56 +0800

    thymeleaf login and register system refine.

--- src/main/java/com/easyrong/wireless/finance/controller/StockController.java
+++ src/main/java/com/easyrong/wireless/finance/controller/StockController.java
@@ -5,10 +5,11 @@ import com.easyrong.wireless.finance.service.NtStockService;
 import com.easyrong.wireless.finance.service.StockService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StockController {
     @Autowired
     private StockService stockService;
@@ -18,8 +19,9 @@ public class StockController {
 
     @ApiOperation(value = "张家港A股19家上市企业", notes = "张家港A股19家上市企业股票信息列表")
     @RequestMapping("/stock")
-    public Object getStockInfo() {
-        return new BaseRspEntity(stockService.getStockList());
+    public Object getStockInfo(Model model) {
+        model.addAttribute("stock", stockService.getStockList());
+        return "stock";
     }
 
     @ApiOperation(value = "张家港新三板挂牌企业", notes = "张家港新三板挂牌企业股票信息列表")
