246ef0cd04e4fed61894206e996dfe6cc7c3cf6c
宋军鹏 <sjp@xinlonghang.cn>
2017-10-20 12:50:58 +0800

    数据存储。

--- sql/zjg_finance.sql
+++ sql/zjg_finance.sql
@@ -1,42 +1,41 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : db_dev
  Source Server Type    : MySQL
- Source Server Version : 50718
- Source Host           : localhost
+ Source Server Version : 50622
+ Source Host           : 172.16.11.120
  Source Database       : zjg_finance
 
  Target Server Type    : MySQL
- Target Server Version : 50718
+ Target Server Version : 50622
  File Encoding         : utf-8
 
- Date: 10/18/2017 09:58:13 AM
+ Date: 10/20/2017 12:49:48 PM
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `api_user`
+--  Table structure for `api_authority`
 -- ----------------------------
-DROP TABLE IF EXISTS `api_user`;
-CREATE TABLE `api_user` (
+DROP TABLE IF EXISTS `api_authority`;
+CREATE TABLE `api_authority` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(250) NOT NULL COMMENT '账户名称',
-  `password` varchar(50) NOT NULL COMMENT '用户密码',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `name` varchar(250) NOT NULL COMMENT '权限名称',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  `value` varchar(50) NOT NULL COMMENT '权限值',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_api_user_name` (`name`) USING BTREE,
-  KEY `idx_api_user_create_time` (`create_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='API用户信息表';
+  UNIQUE KEY `UK_7r9q0ughot4woggkv1aw99vlp` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `api_user`
+--  Records of `api_authority`
 -- ----------------------------
 BEGIN;
-INSERT INTO `api_user` VALUES ('1', 'admin', 'admin', '2017-10-17 09:51:30', '2017-10-17 09:51:32'), ('2', 'roc', 'roc', '2017-10-17 13:18:21', '2017-10-17 13:18:23');
+INSERT INTO `api_authority` VALUES ('1', '2017-10-18 13:24:11', '访问user微服务', '2017-10-18 13:24:14', 'user-service');
 COMMIT;
 
 -- ----------------------------
@@ -45,30 +44,81 @@ COMMIT;
 DROP TABLE IF EXISTS `api_role`;
 CREATE TABLE `api_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
   `name` varchar(250) NOT NULL COMMENT '角色名称',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
   `value` varchar(50) NOT NULL COMMENT '角色值',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_api_role_name` (`name`) USING BTREE,
-  KEY `idx_api_role_create_time` (`create_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='API角色信息表';
+  UNIQUE KEY `UK_7hwvya4jjpx053h47s3qlsno9` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `api_authority`
+--  Records of `api_role`
 -- ----------------------------
-DROP TABLE IF EXISTS `api_authority`;
-CREATE TABLE `api_authority` (
+BEGIN;
+INSERT INTO `api_role` VALUES ('1', '2017-10-18 13:26:39', '管理员', '2017-10-18 13:26:41', 'ROLE_ADMIN'), ('2', '2017-10-18 13:27:36', '普通用户', '2017-10-18 13:27:38', 'ROLE_USER');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `api_role_authoritys`
+-- ----------------------------
+DROP TABLE IF EXISTS `api_role_authoritys`;
+CREATE TABLE `api_role_authoritys` (
+  `api_role_entity_id` bigint(20) NOT NULL COMMENT '主键',
+  `authoritys_id` bigint(20) NOT NULL COMMENT '主键',
+  PRIMARY KEY (`api_role_entity_id`,`authoritys_id`),
+  KEY `FKqy4o5e6teyb1ti3oo395d1g0e` (`authoritys_id`),
+  CONSTRAINT `FKbetiwbwwe1u95ap5n6cvl6x1g` FOREIGN KEY (`api_role_entity_id`) REFERENCES `api_role` (`id`),
+  CONSTRAINT `FKqy4o5e6teyb1ti3oo395d1g0e` FOREIGN KEY (`authoritys_id`) REFERENCES `api_authority` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `api_role_authoritys`
+-- ----------------------------
+BEGIN;
+INSERT INTO `api_role_authoritys` VALUES ('1', '1');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `api_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `api_user`;
+CREATE TABLE `api_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(250) NOT NULL COMMENT '角色名称',
-  `value` varchar(50) NOT NULL COMMENT '角色值',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `name` varchar(250) NOT NULL COMMENT '用户名称',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_api_authority_name` (`name`) USING BTREE,
-  KEY `idx_api_authority_create_time` (`create_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='API权限信息表';
+  UNIQUE KEY `UK_scb81k0sobpewfaxhwyquccop` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Records of `api_user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `api_user` VALUES ('1', '2017-10-18 13:16:52', 'admin', 'admin', '2017-10-18 13:16:55'), ('2', '2017-10-18 13:17:08', 'roc', 'roc', '2017-10-18 13:17:15');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `api_user_roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `api_user_roles`;
+CREATE TABLE `api_user_roles` (
+  `api_user_entity_id` bigint(20) NOT NULL COMMENT '主键',
+  `roles_id` bigint(20) NOT NULL COMMENT '主键',
+  PRIMARY KEY (`api_user_entity_id`,`roles_id`),
+  KEY `FK81n4i83i1ochnbtnq0916uong` (`roles_id`),
+  CONSTRAINT `FK4rk7yiya5b8lvrkt90e7s1f8q` FOREIGN KEY (`api_user_entity_id`) REFERENCES `api_user` (`id`),
+  CONSTRAINT `FK81n4i83i1ochnbtnq0916uong` FOREIGN KEY (`roles_id`) REFERENCES `api_role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `api_user_roles`
+-- ----------------------------
+BEGIN;
+INSERT INTO `api_user_roles` VALUES ('1', '1'), ('2', '2');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `finance_user`
@@ -76,26 +126,25 @@ CREATE TABLE `api_authority` (
 DROP TABLE IF EXISTS `finance_user`;
 CREATE TABLE `finance_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `mobile` bigint(20) NOT NULL COMMENT '手机号码',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `duty` varchar(250) DEFAULT NULL COMMENT '账户职务',
+  `last_login_time` datetime NOT NULL COMMENT '最近登录时间',
+  `mobile` bigint(20) NOT NULL COMMENT '电话号码',
   `name` varchar(250) NOT NULL COMMENT '账户名称',
-  `password` varchar(50) NOT NULL COMMENT '用户密码',
-  `type` tinyint(5) NOT NULL COMMENT '账户类型 1:金融办 2:金融机构',
-  `duty` varchar(250) DEFAULT NULL COMMENT '用户职务',
-  `reg_time` timestamp NULL DEFAULT NULL COMMENT '注册时间',
-  `last_login_time` timestamp NULL DEFAULT NULL COMMENT '最近登录时间',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `reg_time` datetime NOT NULL COMMENT '注册时间',
+  `type` int(11) NOT NULL COMMENT '账户类型 1:金融办 2:金融机构',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_finance_user_mobile` (`mobile`) USING BTREE,
-  KEY `idx_finance_user_reg_time` (`reg_time`),
-  KEY `idx_finance_user_create_time` (`create_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+  UNIQUE KEY `UK_q6ksevh3uivdx0ilaop0b4wj0` (`mobile`),
+  UNIQUE KEY `UK_yydjqhonruvekv1vymusoda1` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Records of `finance_user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `finance_user` VALUES ('78379', '0', '13311112222', 'xlh123456', '0', null, null, null, '2017-10-13 22:52:52', '2017-10-13 22:52:52');
+INSERT INTO `finance_user` VALUES ('1', '2017-10-18 16:17:57', '经理', '2017-10-18 16:18:08', '13817058426', 'roc', 'xlh123456', '2017-10-18 16:18:39', '1', '2017-10-18 16:18:52');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
